4a92ab7adf569254b3f16825e9ef3c251efd1073
cybardev <sheikh@cybar.dev>
2025-02-02 08:25:14 -0400

    re: improve separation of concerns

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from lib.oled import Screen
+import lib.ezFBfont_amstrad_cpc_extended_ascii_08 as font
+from lib.screen import Screen
 from lib.text import Text
 
-screen = Screen()
-txt = Text(screen)
+scrn = Screen(72, 40, x0=28, y0=12)
+txt = Text(scrn, font)
 txt.writelines(["Henlo,", "Wrold~ :3"], border=True, x_offset=1)
